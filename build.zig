99c87dbd333a283198a7c621799a3dda63936ed7
M64GitHub <m64.overdrive@gmail.com>
2025-03-02 19:30:19 +0100

    release fast

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    // const optimize = b.standardOptimizeOption(.{});
     const optimize = std.builtin.OptimizeMode.ReleaseFast;
 
     // Create reSID C++ shared library and C wrapper
