ab4f42c308d0d37991509a55cdf249f636d2dbda
M64GitHub <m64.overdrive@gmail.com>
2025-03-09 20:43:55 +0100

    6510 cpu

--- src/main_6510-cpu-test.zig
+++ src/main_6510-cpu-test.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const CPU = @import("CPU6510/6510.zig").CPU;
+const CPU = @import("6510/6510.zig").CPU;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
